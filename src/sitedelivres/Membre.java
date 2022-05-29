1d34ebd8871a151011dad9fb8d6cbcc5b51956ee
CarlCloutierGosselin <105396290+CarlCloutierGosselin@users.noreply.github.com>
2022-05-29 14:01:45 -0400

    maj des preconditions et modif profil

--- src/sitedelivres/Membre.java
+++ src/sitedelivres/Membre.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Membre extends Utilisateur {
-    
+
     private Date dateInscription;
     private String courriel;
     private String noTelephone;
@@ -14,7 +14,7 @@ public class Membre extends Utilisateur {
     private StatutMembre statut;
     private Set<Signalement> signalements = new HashSet(0);
     private Set<Annonce> annonces = new HashSet(0);
-    
+
     public Membre(Date dateInscription, String courriel, String noTelephone, String adresse, StatutMembre statut, String idUtilisateur, String motPasse, String nom, String prenom) {
         super(idUtilisateur, motPasse, nom, prenom);
         this.dateInscription = dateInscription;
@@ -23,53 +23,66 @@ public class Membre extends Utilisateur {
         this.adresse = adresse;
         this.statut = statut;
     }
-    
+
+    public void modifierProfil(String courriel, String noTelephone, String adresse) {
+        if (!this.estAuthentifie) {
+            System.out.println("Le membre d'id=" + idUtilisateur + " doit être authentifié pour effectuer une publication");
+            return;
+        }
+
+        this.courriel = courriel;
+        this.noTelephone = noTelephone;
+        this.adresse = adresse;
+    }
+
     public Annonce publierAnnonce(Set<Description> descriptions, Set<Photo> photos) {
         if (!this.estAuthentifie) {
-            System.out.println("Le membre doit être authentifié pour effectuer une publication");
+            System.out.println("Le membre d'id=" + idUtilisateur + "doit être authentifié pour effectuer une publication");
             return null;
+        } else if (this.getStatut() != StatutMembre.ACTIF) {
+            System.out.println("Le membre d'id=" + idUtilisateur + " doit être actif pour publier une annonce");
         }
-        
+
         Annonce nouvelleAnnonce = new Annonce(new Date(), StatutAnnonce.PUBLIEE, this);
         for (Description description : descriptions) {
             description.setAnnonce(nouvelleAnnonce);
         }
-        
+
         for (Photo photo : photos) {
             photo.setAnnonce(nouvelleAnnonce);
         }
-        
+
         nouvelleAnnonce.setPhotos(photos);
         annonces.add(nouvelleAnnonce);
-        
+
         return nouvelleAnnonce;
     }
-    
+
     public Signalement signalerAnnonce(Annonce annonce, String objet) {
         if (!this.estAuthentifie) {
             System.out.println("Le membre d'id=" + getIdUtilisateur() + " doit être authentifié pour signaler une annonce");
             return null;
         }
-        
+
         Signalement signalement = new Signalement(new Date(), objet, null, annonce, null, annonce.getMembre());
         annonce.getSignalements().add(signalement);
-        
+
         annonce.getMembre().ajouterSignalement();
-        
+
         return signalement;
     }
-    
+
     public ExperienceClient commenterExperienceUtilisateur(int noteLivre, int noteVendeur, String commentaire, int evaluation, Annonce annonce) {
         if (!this.estAuthentifie) {
             System.out.println("Le membre d'id=" + getIdUtilisateur() + " doit être authentifié pour commenter une experience client");
             return null;
         }
-        
+
         ExperienceClient experienceClient = new ExperienceClient(noteLivre, noteVendeur, commentaire, new Date(), evaluation, this, annonce);
-        
+
         return experienceClient;
     }
-    
+
     public Set<Annonce> rechercherAnnonces(Set<Description> descriptions, String titre) {
         Set<Annonce> annoncesTrouvees = new HashSet(0);
         for (Description description : descriptions) {
@@ -78,10 +91,10 @@ public class Membre extends Utilisateur {
                 annoncesTrouvees.add(description.getAnnonce());
             }
         }
-        
+
         return annoncesTrouvees;
     }
-    
+
     public Set<Annonce> rechercherAnnonces(Set<Description> descriptions, Cours cours) {
         Set<Annonce> annoncesTrouvees = new HashSet(0);
         for (Description description : descriptions) {
@@ -90,10 +103,10 @@ public class Membre extends Utilisateur {
                 annoncesTrouvees.add(description.getAnnonce());
             }
         }
-        
+
         return annoncesTrouvees;
     }
-    
+
     public Set<Annonce> rechercherAnnonces(Set<Description> descriptions, float prixMin, float prixMax) {
         Set<Annonce> annoncesTrouvees = new HashSet(0);
         for (Description description : descriptions) {
@@ -103,10 +116,10 @@ public class Membre extends Utilisateur {
                 annoncesTrouvees.add(description.getAnnonce());
             }
         }
-        
+
         return annoncesTrouvees;
     }
-    
+
     public Set<Annonce> rechercherAnnonces(Set<Description> descriptions, Categorie categorie) {
         Set<Annonce> annoncesTrouvees = new HashSet(0);
         for (Description description : descriptions) {
@@ -115,111 +128,108 @@ public class Membre extends Utilisateur {
                 annoncesTrouvees.add(description.getAnnonce());
             }
         }
-        
+
         return annoncesTrouvees;
     }
-    
+
     public void consulterAnnonce(Annonce annonce) {
         System.out.println(annonce);
     }
-    
+
     public void modifierAnnonce(Annonce annonce, Set<Photo> photos) {
         if (annonces.contains(annonce)) {
             annonce.setPhotos(photos);
-        }
-        else {
+        } else {
             System.out.println("Le membre d'id=" + getIdUtilisateur() + " ne peut modifier que ses annonces");
         }
     }
-    
-    public void modifierAnnonce(Description description, String etat, float prix, String resume){
-        if (annonces.contains(description.getAnnonce())){
+
+    public void modifierAnnonce(Description description, String etat, float prix, String resume) {
+        if (annonces.contains(description.getAnnonce())) {
             description.setEtat(etat);
             description.setPrix(prix);
             description.setResume(resume);
-        }
-        else {
+        } else {
             System.out.println("Le membre d'id=" + getIdUtilisateur() + " ne peut modifier que ses annonces");
         }
     }
-    
+
     public void desactiverAnnonce(Annonce annonce) {
         if (this == annonce.getMembre()) {
             annonce.setStatut(StatutAnnonce.DESACTIVEE);
-        }
-        else {
+        } else {
             System.out.println("Le membre d'id=" + getIdUtilisateur() + " ne peut modifier que ses annonces");
         }
     }
-    
+
     public void ajouterSignalement() {
         nbSignalements++;
     }
-    
+
     public Date getDateInscription() {
         return dateInscription;
     }
-    
+
     public void setDateInscription(Date dateInscription) {
         this.dateInscription = dateInscription;
     }
-    
+
     public String getCourriel() {
         return courriel;
     }
-    
+
     public void setCourriel(String courriel) {
         this.courriel = courriel;
     }
-    
+
     public String getNumeroTelephone() {
         return noTelephone;
     }
-    
+
     public void setNumeroTelephone(String numeroTelephone) {
         this.noTelephone = numeroTelephone;
     }
-    
+
     public String getAdresse() {
         return adresse;
     }
-    
+
     public void setAdresse(String adresse) {
         this.adresse = adresse;
     }
-    
+
     public StatutMembre getStatut() {
         return statut;
     }
-    
+
     public void setStatut(StatutMembre statut) {
         this.statut = statut;
     }
-    
+
     public int getNbSignalements() {
         return nbSignalements;
     }
-    
+
     public void setNbSignalements(int nbSignalements) {
         this.nbSignalements = nbSignalements;
     }
-    
+
     public Set<Signalement> getSignalements() {
         return signalements;
     }
-    
+
     public void setSignalements(Set<Signalement> signalements) {
         this.signalements = signalements;
     }
-    
+
     public Set<Annonce> getAnnonces() {
         return annonces;
     }
-    
+
     public void setAnnonces(Set<Annonce> annonces) {
         this.annonces = annonces;
     }
-    
+
     @Override
     public String toString() {
         return "Membre{" + "dateInscription=" + dateInscription
@@ -228,5 +238,5 @@ public class Membre extends Utilisateur {
                 + ", statut=" + statut + ", signalements=" + signalements
                 + ", annonces=" + annonces + '}';
     }
-    
+
 }
